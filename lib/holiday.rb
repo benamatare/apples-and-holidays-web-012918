4d7dd36f58a0ee34529cb2a1b501bc9842851f3d
Ben Matare <ben.a.matare@gmail.com>
2018-01-25 03:40:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  (holiday_hash)
+
     holiday_hash.each do |season, holiday|
       puts season.to_s.capitalize + ":"
       holiday.each do |day, supply|
