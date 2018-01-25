f61d8f5070bdea91076c70ddf1c0e3b67bc11ab2
Ben Matare <ben.a.matare@gmail.com>
2018-01-25 03:42:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
       puts season.to_s.capitalize + ":"
       holiday.each do |day, supply|
-        puts " " + day.to_s.gsub(/[^0-9A-Za-z]/, " ").capitalize.split(/\s+/).each { |word| word.capitalize!}.join(' ') + ": " + supply.join(', ')
+        puts "  " + day.to_s.gsub(/[^0-9A-Za-z]/, " ").capitalize.split(/\s+/).each { |word| word.capitalize!}.join(' ') + ": " + supply.join(', ')
 
       end
 
